8ec7a5d09fef281218620da3a5a496d738ca1e3c
Guillaume G <guillaume.galliano@gmail.com>
2019-01-26 14:06:48 -0500

    Added the redirection from the example to the picker activity

--- example/src/main/java/com/unsplash/pickerandroid/example/MainActivity.kt
+++ example/src/main/java/com/unsplash/pickerandroid/example/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.unsplash.pickerandroid.example
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.unsplash.pickerandroid.photopicker.presentation.PickerActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // on the pick button click, we start the library picker activity
+        // we are expecting a result from it so we start it for result
+        main_pick_button.setOnClickListener {
+            startActivityForResult(
+                PickerActivity.getStartingIntent(
+                    this,
+                    !main_single_radio_button.isChecked
+                ), REQUEST_CODE
+            )
+        }
+    }
+
+    // here we are receiving the result from the picker activity
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE) {
+            // TODO get images selected here
+        }
+    }
+
+    companion object {
+        // dummy request code to identify the request
+        private const val REQUEST_CODE = 123
     }
 }
--- photopicker/src/main/java/com/unsplash/pickerandroid/photopicker/presentation/PickerActivity.kt
+++ photopicker/src/main/java/com/unsplash/pickerandroid/photopicker/presentation/PickerActivity.kt
@@ -5,20 +5,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.unsplash.pickerandroid.photopicker.R
+import kotlinx.android.synthetic.main.activity_picker.*
 
 class PickerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picker)
+        picker_back_image_view.setOnClickListener { onBackPressed() }
     }
 
     companion object {
+        private const val EXTRA_IS_MULTIPLE = "EXTRA_IS_MULTIPLE"
+
         /**
+         * @param callingContext the calling context
+         * @param isMultipleSelection true if multiple selection, false otherwise
+         *
          * @return the intent needed to come to this activity
          */
-        fun getStartingIntent(callingContext: Context): Intent {
-            return Intent(callingContext, PickerActivity::class.java)
+        fun getStartingIntent(callingContext: Context, isMultipleSelection: Boolean): Intent {
+            val intent = Intent(callingContext, PickerActivity::class.java)
+            intent.putExtra(EXTRA_IS_MULTIPLE, isMultipleSelection)
+            return intent
         }
     }
 }
